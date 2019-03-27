cc544895993cfe374b7e02e1a5f98435b3ce9cec
Mike Harris <MikeMKH@users.noreply.github.com>
2019-03-27 18:38:11 -0500

    refactor exercises from chapter 8

--- ch08/exercises.R
+++ ch08/exercises.R
@@ -1,8 +1,9 @@
+# 8.1
 data("quakes")
 write.table(x = quakes[quakes$mag >= 5,],
-            file = "./q5.txt", sep = "!", row.names = FALSE
+            file = "q5.txt", sep = "!", row.names = FALSE
 )
-q5.dframe <- read.table(file = "./q5.txt", sep = "!", header = TRUE)
+q5.dframe <- read.table(file = "q5.txt", sep = "!", header = TRUE)
 
 # install.packages("car")
 library("car")
@@ -11,4 +12,10 @@ data("Duncan")
 library("ggplot2")
 ggplot(data = Duncan, aes(education, income)) +
   geom_point() +
-  xlim(0, 100)
+  xlim(0, 100) +
+  ggsave(filename = "duncan.png")
+
+unlink("q5.txt")
+unlink("duncan.png")
+
+# skip c
