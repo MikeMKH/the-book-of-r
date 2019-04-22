3b4241098f2d205ce03ee0c1f969390730b130bb
Mike Harris <MikeMKH@users.noreply.github.com>
2019-04-22 06:11:23 -0500

    add exercises from chapter 13.1

---
+++ ch13/exercises.R
@@ -0,0 +1,9 @@
+# 13.1
+  # a.i numeric discrete
+  # a.ii categorical ordinal
+  # a.iii numeric continuous
+  # a.iv categorical ordinal
+  # a.v categorical nominal
+  # a.vi numeric continuous
+
+# skipped b
