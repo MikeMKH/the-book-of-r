0a9f8526cc81ffca96e7d889a8193e3991c3ea1b
Mike Harris <MikeMKH@users.noreply.github.com>
2019-04-22 06:29:33 -0500

    add exercises from chapter 13.2

--- ch13/exercises.R
+++ ch13/exercises.R
@@ -7,3 +7,22 @@
   # a.vi numeric continuous
 
 # skipped b
+
+# 13.2
+round(mean(quakes$depth >= 300), 2)
+
+mean(quakes$mag[quakes$depth >= 300])
+median(quakes$mag[quakes$depth >= 300])
+
+# c is dumb using tapply
+tapply(chickwts$weight, chickwts$feed, mean)
+
+# numeric discrete and categorical nominal
+ctab <- table(InsectSprays$count)
+ctab[ctab == max(ctab)]
+
+tapply(InsectSprays$count, InsectSprays$spray, sum)
+
+# skipped g
+tapply(InsectSprays$count, InsectSprays$spray,
+       function(x) round(mean(x >= 5) * 100.0, 2))
