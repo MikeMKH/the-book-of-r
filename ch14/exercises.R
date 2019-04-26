a200c5ff1e1e89f836dc7ebc93f2afa9055077a3
Mike Harris <MikeMKH@users.noreply.github.com>
2019-04-26 06:36:37 -0500

    add exercises from 14.1 for USArrest dataset

--- ch14/exercises.R
+++ ch14/exercises.R
@@ -10,3 +10,31 @@ pie(insect.count, labels = insect.spray, main = "Inspects by Spray Type")
 library(ggplot2)
 qplot(InsectSprays$spray, InsectSprays$count, geom = "boxplot",
       xlab = "Type", ylab = "Count", main = "Inspects by Spray Type")
+
+qplot(USArrests$UrbanPop, geom = "blank",
+      main = "US Urban Population", xlab = "proporation urban") +
+  geom_histogram(color = "blue", fill = "white",
+                 breaks = seq(0, 100, 10), closed = "right") +
+  geom_vline(mapping = aes(xintercept = c(
+    quantile(USArrests$UrbanPop, .25),
+    mean(USArrests$UrbanPop),
+    quantile(USArrests$UrbanPop, .75)),
+    linetype = factor(c("1st Q", "median", "3rd Q"))),
+    show.legend = TRUE) +
+  scale_linetype_manual(values = 3:5) +
+  labs(linetype = "")
+
+barplot(t(as.matrix(USArrests[,-3])), names.arg = state.abb,
+        horiz = TRUE, las = 1, legend.text = c("murder","rape","assault"))
+
+urbancat <- rep(0, 50)
+urbancat[USArrests$UrbanPop > median(USArrests$UrbanPop)] <- 1
+urbancat <- factor(urbancat)
+
+g <- USArrests[, -3]
+g$urbancat <- urbancat
+
+library(GGally)
+ggpairs(g, aes(col = urbancat), axisLabels = "internal")
+
+# skipped i - k
