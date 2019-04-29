b2003d9b8621879902c49a9f1b5ed6bf4583350c
Mike Harris <MikeMKH@users.noreply.github.com>
2019-04-29 06:24:02 -0500

    add exercises from chapter 15.1

---
+++ ch15/exercises.R
@@ -0,0 +1,14 @@
+# 15.1
+4/52 # Pr(ace)
+1/52 # Pr(4 of spades)
+
+13/52 # Pr(club | red)
+
+13/52 # Pr(club)
+13/51 # Pr(club | red)
+
+(4 * 3)/52      # Pr(face)
+(2 * 13)/52     # Pr(black)
+((4 * 3)/2)/ 52 # Pr(face | black)
+(((4 * 3)/2)/ 52) *
+  ((2 * 13)/52) # Pr(face and black)
