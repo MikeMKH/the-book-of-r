8629ebda562c8d9c69132ee8ef27362a5d5e3603
Mike Harris <MikeMKH@users.noreply.github.com>
2019-05-03 06:37:53 -0500

    add example from chapter 16.1

--- ch16/exercises.R
+++ ch16/exercises.R
@@ -1,18 +1,31 @@
+# probability of flipping heads 3 times in 5 flips
+dbinom(3, 5, 0.5)
+# probability of flipping heads 5 times in a row
+dbinom(5, 5, 0.5)
+# ... 100 times
+dbinom(100, 100, 0.5)
+
 # 16.1
-barplot(dbinom(x = 1:13, size = 13, prob = 0.75),
-        names.arg = 0:13, space = 0,
+stations = 13
+p = 0.75
+
+barplot(dbinom(x = 1:stations, size = stations, prob = p),
+        names.arg = 0:stations, space = 0,
         xlab = "x", ylab = "Pr(X = x)")
 
-dbinom(13, 13, 0.75)
+dbinom(x = 13, size = stations, prob = p)
 
-1 - pbinom(q = 9, size = 13, prob = 0.75)
+1 - pbinom(q = 9, size = stations, prob = p)
 
-sum(dbinom(8:11, 13, 0.75))
-pbinom(11, 13, 0.75) - pbinom(7, 13, 0.75)
+sum(dbinom(8:11, stations, p))
+pbinom(11, stations, p) - pbinom(7, stations, p)
 
-pbinom(8, 13, 0.75)
+pbinom(8, stations, p)
 
-visits <- rbinom(10, 13, 0.75)
+visits <- rbinom(10, stations, p)
 
-mu.X <- 13 * 0.75
-sigma.X <- sqrt(mu.X * .25)
+# probability of n successes
+mu.X <- stations * p
+
+# variance of n successes
+sigma.X <- sqrt(mu.X * (1 - p))
