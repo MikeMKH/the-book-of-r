db4643c5fa244af8a4faa5a43a4dca7be6e1e0e5
Mike Harris <MikeMKH@users.noreply.github.com>
2019-05-13 06:34:46 -0500

    add exercise from chapter 17.2

--- ch17/exercises.R
+++ ch17/exercises.R
@@ -27,3 +27,29 @@ qnorm(.1, mean = survey.pi,
       sd = sqrt(survey.pi * (1 - survey.pi) / survey.total))
 
 # skip g - h
+
+# 17.2
+runner.n <- 34
+runner.mean <- 14.22
+runner.sd <- 2.9
+runner.se <- runner.sd / sqrt(runner.n)
+
+runner.mean + c(-1, 1) * qnorm(.9) * runner.se
+runner.mean + c(-1, 1) * qt(.9, df = runner.n - 1) * runner.se
+
+runner.sample <- rnorm(n = 5, mean = runner.mean, sd = runner.se)
+runner.sample
+
+hand.n <- 400
+hand.left <- 37
+hand.ambidextrous <- 11
+
+hand.hat <- hand.left / hand.n
+hand.se <- sqrt(hand.hat * (1 - hand.hat) / hand.n)
+hand.hat + c(-1, 1) * qnorm(1 - .01 / 2) * hand.se
+
+hand.hat <- (hand.left + hand.ambidextrous) / hand.n
+hand.se <- sqrt(hand.hat * (1 - hand.hat) / hand.n)
+hand.hat + c(-1, 1) * qnorm(1 - .01 / 2) * hand.se
+
+# skipped e - f
