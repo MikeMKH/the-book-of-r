0c716f596874f5ffe684bb9d2be6272d68e4d784
Mike Harris <MikeMKH@users.noreply.github.com>
2019-06-27 06:33:36 -0500

    add exercises e-h from chapter 24.1

--- ch24/exercises.R
+++ ch24/exercises.R
@@ -31,3 +31,45 @@ d <- ggplot(cereal, aes(x = protein, y = calories)) +
   facet_wrap(~mfr) +
   labs(x = "Protein", y = "Calories", col = "Sugars", shape = "Shelf", size = "Sodium")
 d
+
+library(ggplot2)
+library("car")
+?Salaries
+summary(Salaries)
+pairs(Salaries)
+
+gg1 <- ggplot(Salaries, aes(x = yrs.service, y = salary, col = sex)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  labs(x = "Years of Service", y = "Salary", col = "Gender")
+gg1
+
+gg2 <- ggplot(Salaries, aes(x = rank, y = salary, col = sex)) +
+  geom_boxplot() +
+  labs(x = "Rank", y = "Salary", col = "Gender")
+gg2
+
+gg3 <- ggplot(Salaries, aes(x = discipline, y = salary, col = sex)) +
+  geom_boxplot() +
+  labs(x = "Discipline", y = "Salary", col = "Gender")
+gg3
+
+gg4 <- ggplot(Salaries, aes(x = salary, fill = rank)) +
+  geom_density(alpha = 0.3) +
+  labs(x = "Salary", y = "Kernel Estimate", fill = "Rank")
+gg4
+
+library("gridExtra")
+grid.arrange(gg1, gg2, gg3, gg4)
+
+h1 <- ggplot(Salaries, aes(x = salary, fill = sex)) +
+  geom_density(alpha = 0.7) +
+  facet_wrap(~rank) +
+  labs(x = "Salary", y = "Kernel Estimate", fill = "Gender")
+h1
+
+h2 <- ggplot(Salaries, aes(x = yrs.service, y = salary, col = sex)) +
+  geom_point() +
+  facet_grid(discipline~rank, scales = "free_x") +
+  labs(x = "Years of Service", y = "Salary", col = "Gender")
+h2
